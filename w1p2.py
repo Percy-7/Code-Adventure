e0116d064c110e62fd232f8b424aa42776255faf
Percy-7 <percyjackson.piper@gmail.com>
2026-06-09 20:51:01 +0530

    doneeee, shit code tho, fixing it rn

--- w1p2.py
+++ w1p2.py
@@ -4,17 +4,8 @@ with open('w1p2.txt') as input:
 
 puzzle_input[3], puzzle_input[2] = puzzle_input[2], puzzle_input[3]
 
-dict_list = [dict(zip(puzzle_input[i+1], puzzle_input[0])) for i in range(3)]
-# dict_list = dict(*dict(zip(puzzle_input[i+1], puzzle_input[0])) for i in range(3))
-char_dict = {weird_char: char for i in range(3) for weird_char, char in zip(puzzle_input[i+1], puzzle_input[i]) }
-print(char_dict)
-
-# dict_list = {puzzle_input[i+1]: puzzle_input[0] for i in range(3)}
-# test= {**d for d in dict_list}
+char_dict = {weird_char: char for i in range(4) for weird_char, char in zip(puzzle_input[i+1], puzzle_input[0]) }
 directions_dict =  {char: 1j**i for i in range(4) for char in puzzle_input[i]}
-
-# print(dict_list)
-# print(directions_dict)
 
 char_grid = puzzle_input[5:]
 
@@ -23,13 +14,30 @@ def in_bounds(coords: complex) -> bool:
 def get_element(coords:complex) -> str:
     return char_grid[int(coords.imag)][int(coords.real)]
 
+print(char_dict)
+
 pointer: complex = 0+0j
 direction: complex = 1j
 visited_coords: set = set()
 
-# translated_phrase: str = ''
-# while True:
-#     visited_coords.add(pointer)
-#     translated_phrase += dict_list.get(get_element(pointer), get_element(pointer))
+translated_phrase: str = ''
+while True:
+    current_char = get_element(pointer)
+    if current_char == ' ':
+        translated_phrase += ' '
+        pointer+=direction
+        continue
+    visited_coords.add(pointer)
+    translated_phrase += char_dict.get(current_char, current_char)
+    try:
+        direction = directions_dict[current_char]
+    except KeyError:
+        pass
+    pointer+=direction
+    if not in_bounds(pointer) or pointer in visited_coords:
+        break
 
-# print(dict_list)
+    print(translated_phrase)
+
+print('\n\n')
+print(translated_phrase)
