d17798f27a596b7f4c901775102b99ccf6820883
Percy-7 <percyjackson.piper@gmail.com>
2026-06-09 21:10:41 +0530

    done, i think it's pretty enough too. rearranged some statements, and simplified logic down.

--- w1p2.py
+++ w1p2.py
@@ -1,43 +1,29 @@
 with open('w1p2.txt') as input:
-    # puzzle_input: list[str] = [''.join([j for j in i.strip('\n') if j not in [',', '.', '-', "'"]]) for i in input]
     puzzle_input: list[str] = [i.strip('\n') for i in input]
 
 puzzle_input[3], puzzle_input[2] = puzzle_input[2], puzzle_input[3]
+char_grid: list[str] = puzzle_input[5:]
+char_dict: dict[str, str] = {
+    weird_char: char for i in range(4) for weird_char, char in zip(puzzle_input[i + 1], puzzle_input[0])
+}
+directions_dict: dict[str, complex] = {char: 1j**i for i in range(4) for char in puzzle_input[i]}
 
-char_dict = {weird_char: char for i in range(4) for weird_char, char in zip(puzzle_input[i+1], puzzle_input[0]) }
-directions_dict =  {char: 1j**i for i in range(4) for char in puzzle_input[i]}
-
-char_grid = puzzle_input[5:]
 
 def in_bounds(coords: complex) -> bool:
-    return(coords.real in range(len(char_grid[0])) and coords.imag in range(len(char_grid)))
-def get_element(coords:complex) -> str:
-    return char_grid[int(coords.imag)][int(coords.real)]
+    return coords.real in range(len(char_grid[0])) and coords.imag in range(len(char_grid))
 
-print(char_dict)
 
-pointer: complex = 0+0j
+pointer: complex = 0 + 0j
 direction: complex = 1j
 visited_coords: set = set()
-
 translated_phrase: str = ''
-while True:
-    current_char = get_element(pointer)
-    if current_char == ' ':
-        translated_phrase += ' '
-        pointer+=direction
-        continue
+
+while in_bounds(pointer) and pointer not in visited_coords:
+    current_char: str = char_grid[int(pointer.imag)][int(pointer.real)]
     visited_coords.add(pointer)
     translated_phrase += char_dict.get(current_char, current_char)
-    try:
-        direction = directions_dict[current_char]
-    except KeyError:
-        pass
-    pointer+=direction
-    if not in_bounds(pointer) or pointer in visited_coords:
-        break
+    direction = directions_dict.get(current_char, direction)
+    pointer += direction
 
-    print(translated_phrase)
-
-print('\n\n')
-print(translated_phrase)
+translated_words: list[str] = translated_phrase.split()
+print(translated_words[translated_words.index('NAMED') + 1])
