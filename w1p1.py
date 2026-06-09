13c91ead21fa3d5ec706d8e9416c3a67e0c3b95c
Percy-7 <percyjackson.piper@gmail.com>
2026-06-09 19:43:47 +0530

    done

--- w1p1.py
+++ w1p1.py
@@ -1,20 +1,11 @@
 with open('w1.txt') as input:
-    puzzle_input = [''.join([j for j in i.strip('\n') if j not in [',','.','-', '\'']]) for i in input]
+    puzzle_input: list[str] = [''.join([j for j in i.strip('\n') if j not in [',', '.', '-', "'"]]) for i in input]
 
 backward_dict: dict[str, str] = dict(zip(puzzle_input[1], puzzle_input[0], strict=True))
 
-words = ''.join([text[::-1] if i%2==0 else text for i, text in enumerate(puzzle_input[2:])])
-words_translated = ''.join([backward_dict.get(i,i) for i in words])
-words_translated_list = words_translated.split()
+words: str = ''.join([text[::-1] if i % 2 == 0 else text for i, text in enumerate(puzzle_input[2:])])
+words_translated: str = ''.join([backward_dict.get(i, i) for i in words])
+words_translated_list: list[str] = words_translated.split()
+five_letter_words: list[str] = sorted({i for i in words_translated_list if len(i) == 5})
 
-five_letter_words = sorted({i for i in words_translated_list if len(i) == 5})
 print(five_letter_words[19])
-
-# checked = set()
-# count: int = 0
-# for i in five_letter_words:
-#     if i not in checked:
-#         checked.add(i)
-#         count+=1
-#     if count == 20:
-#         print(i)
