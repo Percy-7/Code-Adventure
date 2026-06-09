e80069142120e64ec6e8b669d8b1db5f063b8ae7
Percy-7 <percyjackson.piper@gmail.com>
2026-06-09 20:14:23 +0530

    got basics done

--- w1p2.py
+++ w1p2.py
@@ -1,12 +1,11 @@
 with open('w1p2.txt') as input:
     puzzle_input: list[str] = [''.join([j for j in i.strip('\n') if j not in [',', '.', '-', "'"]]) for i in input]
 
-# print(puzzle_input[:4])
+puzzle_input[3], puzzle_input[2] = puzzle_input[2], puzzle_input[3]
+
 dict_list = [dict(zip(puzzle_input[i+1], puzzle_input[0])) for i in range(3)]
-# sets_list = [{puzzle_input[i+1] for i in range(3)}]
-# directions_dict =  dict([(char, i) for i in range(3) for char in puzzle_input[i+1]])
-directions_dict =  {char:i for i in range(3) for char in puzzle_input[i+1]}
+directions_dict =  {char: 1j**i for i in range(4) for char in puzzle_input[i]}
 
-# print(dict_list[0])
+# print(dict_list)
+# print(directions_dict)
 
-print(directions_dict)
