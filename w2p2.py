bf98581a3359997ff7695689e573273c7c2f6859
Percy-7 <percyjackson.piper@gmail.com>
2026-06-13 01:59:38 +0530

    it does finish ruining before the heat death!
    Also crazy cache info: CacheInfo(hits=468468653, misses=8, maxsize=None, currsize=8)
    it's still super shit tho lmao, p2 runs in like 2-3 minutes prolly.

--- w2p2.py
+++ w2p2.py
@@ -10,10 +10,9 @@ def batches_of_two(count: int, char: str) -> str:
     return f'2{char}' * (count // 2) + f'1{char}' * (count % 2 != 0)
 
 
-for i in range(10):
+for i in range(65):
     text_chunks = (batches_of_two(len(i), i[0]) for i in findall(r'1+|2+', input_sequence))
     input_sequence = ''.join(text_chunks)
 
 
-print(input_sequence)
 print(len(findall(r'1{3}|2{3}', input_sequence)))
