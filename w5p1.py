c3ab8ceaee314f5226f0d715d94c6039a4c8a3f8
Percy-7 <percyjackson.piper@gmail.com>
2026-08-31 10:31:24 +0530

    cleaned up code. going to get rid of debug statements.
    gonna check later if there's more scope for improvement.

--- w5p1.py
+++ w5p1.py
@@ -1,4 +1,3 @@
-import itertools
 import re
 from collections import defaultdict
 
@@ -8,15 +7,6 @@ with open('w5p1.txt') as input:
 
 length = len(puzzle_input[0])
 wall_coords: set[complex] = {complex(*divmod(i, length)[::-1]) for i in (i.start() for i in re.finditer(r'[^07]', ''.join(puzzle_input)))}
-
-# wall_directions: dict[str, tuple[complex, complex]] = {
-#     '1': (1, -1),
-#     '2': (1j, -1j),
-#     '3': (-1, 1j),
-#     '4': (-1, -1j),
-#     '5': (1, -1j),
-#     '6': (1, 1j),
-# }
 
 wall_directions: dict[str, frozenset[complex]] = {
     '1': frozenset([1, -1]),
@@ -29,11 +19,6 @@ wall_directions: dict[str, frozenset[complex]] = {
 
 rev_wall_directions: dict[frozenset[complex], str] = {v: k for k, v in wall_directions.items()}
 
-# complex_to_wasd: dict[complex, str] = {1: 'd', -1 : 'a', 1j: 's', -1j: 'w'}
-
-# rev_wall_directions: dict[tuple[str, str], str] = {tuple(sorted(complex_to_wasd[i] for i in v)): k for k,v in wall_directions.items()} # type: ignore
-
-# broken_walls_directions: defaultdict[complex, list[str]] = defaultdict(list)
 broken_walls_directions: defaultdict[complex, list[complex]] = defaultdict(list)
 
 def traverse_grid(wall_type: str, wall_coord: complex, entry_direction: complex) -> None:
@@ -42,31 +27,20 @@ def traverse_grid(wall_type: str, wall_coord: complex, entry_direction: complex)
     if wall_type == '7':
         print(f'found broken wall at {wall_coord}')
         broken_walls_directions[wall_coord].append(entry_direction)
-        # broken_walls_directions[wall_coord].append(complex_to_wasd[entry_direction])
-        # print('ran')
         return
     
     wall_coords.discard(wall_coord)
     
-    # exit_direction: complex = next(iter(wall_directions[wall_type] - {entry_direction}))*-1
-    exit_direction: complex = next(iter(set(wall_directions[wall_type]) - {entry_direction}))*-1
+    exit_direction: complex = next(iter(wall_directions[wall_type] - {entry_direction}))*-1
     new_coords = wall_coord + exit_direction
     traverse_grid(puzzle_input[int(new_coords.imag)][int(new_coords.real)], new_coords, exit_direction)
 
 while wall_coords:
     chosen_coord = next(iter(wall_coords))
     wall_type = puzzle_input[int(chosen_coord.imag)][int(chosen_coord.real)]
-    # print((wall_type, chosen_coord, next(iter(wall_directions[wall_type]))))
     entry_direction  = next(iter(wall_directions[wall_type]))
     traverse_grid(wall_type, chosen_coord, entry_direction)
-    traverse_grid(wall_type, chosen_coord, next(iter(set(wall_directions[wall_type]) - {entry_direction})))
+    traverse_grid(wall_type, chosen_coord, next(iter(wall_directions[wall_type] - {entry_direction})))
 
-print(broken_walls_directions)
-
-symbol_map = [(coord, rev_wall_directions[frozenset(directions)]) for coord, directions in broken_walls_directions.items()]
-
-# print(sorted([i[1] for i in symbol_map]))
-
-# print([list(i[1]) for i in itertools.groupby(sorted([i[1] for i in symbol_map]), int)])
-# print(list(map(len, [list(i[1]) for i in itertools.groupby(sorted([i[1] for i in symbol_map]), int)])))
-print(''.join(list(map(str, (map(len, [list(i[1]) for i in itertools.groupby(sorted([i[1] for i in symbol_map]), int)]))))))
+symbols = [rev_wall_directions[frozenset(directions)] for directions in broken_walls_directions.values()]
+print(''.join(map(str, [symbols.count(str(i)) for i in range(1, 7)])))
